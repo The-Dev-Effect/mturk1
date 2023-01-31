2914c5893f04a64624c0fb5d902e9cc0e7ece5f1
Brett Jurman <i.be.brett@gmail.com>
2023-01-31 14:59:47 -0500

    trying mui

--- mturk1/src/App.js
+++ mturk1/src/App.js
@@ -1,24 +1,26 @@
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
 
 function App() {
+  const [count, setCount] = useState(1);
+  let buttons = [];
+  for (let i = 0; i < count; i++) {
+    buttons.push(
+      <p key={i}>
+        <Button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          Count: {count}
+        </Button>
+      </p>
+    );
+  }
   return (
     <div className="App">
-      <p>Hello mturk1</p>
-      <p>Hello mturk1</p>
-      <p>Hello mturk1</p>
-      <p>Hello mturk1</p>
-      <p>Hello mturk1</p>
-      <p>Hello mturk1</p>
-      <p>Hello mturk1</p>
-      <p>Hello mturk1</p>
-      <p>Hello mturk1</p>
-      <p>Hello mturk1</p>
-      <p>Hello mturk1</p>
-      <p>Hello mturk1</p>
-      <p>Hello mturk1</p>
-      <p>Hello mturk1</p>
-      <p>Hello mturk1</p>
-      <p>Hello mturk1</p>
+      {buttons}
       <p>Hello mturk1</p>
     </div>
   );
