3751de7b4ad9bb5933a03eb8c68821e90098a51e
Brett Jurman <i.be.brett@gmail.com>
2023-02-01 10:23:08 -0500

    updating the app

--- mturk1/src/App.js
+++ mturk1/src/App.js
@@ -2,67 +2,49 @@ import "./App.css";
 import React, { useState, useEffect } from "react";
 import Button from "@mui/material/Button";
 
+const getRandomPrompt = () => {
+  const prompts = [
+    "Click many times!",
+    "Click some amount of times!",
+    "Please click a few times.",
+    "Click this?",
+    "What does this do?",
+    "Click for a while.",
+    "What happens if you click?",
+    "Click me.",
+    "???",
+  ];
+  return prompts[Math.floor(Math.random() * prompts.length)];
+};
+
 function App() {
-  const [count, setCount] = useState(1);
-  let buttons = [];
+  const [count, setCount] = useState(0);
+  const [clickTimes, setClickTimes] = useState([]);
+  const [prompt] = useState(getRandomPrompt());
+  const [now] = useState(Date.now());
 
   // Update the submit button
   useEffect(() => {
-    /*
-    const urlParams = new URLSearchParams(window.location.search);
-    console.log(urlParams);
-
-    // create the form element and point it to the correct endpoint
-    const form = document.createElement("form");
-    form.action = new URL(
-      "mturk/externalSubmit",
-      urlParams.get("turkSubmitTo")
-    ).href;
-    form.method = "post";
-
-    // attach the assignmentId
-    const inputAssignmentId = document.createElement("input");
-    inputAssignmentId.name = "assignmentId";
-    inputAssignmentId.value = urlParams.get("assignmentId");
-    inputAssignmentId.hidden = true;
-    form.appendChild(inputAssignmentId);
-
-    // attach data I want to send back
-    // attach data I want to send back
-    const results = document.createElement("input");
-    results.name = "results";
-    results.value = JSON.stringify({
-      count: count,
-    });
-    results.hidden = true;
-    document.getElementById("mturk_form").submit();
-    form.appendChild(results);
-
     // attach the form to the HTML document and trigger submission
-    document.body.appendChild(form);
-    form.submit();*/
     document.getElementById("mturk-result").value = JSON.stringify({
       count: count,
+      clicksTimes: clickTimes,
+      prompt: prompt,
+      now: now,
     });
   });
 
-  for (let i = 0; i < count; i++) {
-    buttons.push(
-      <p key={i}>
-        <Button
-          onClick={() => {
-            setCount(count + 1);
-          }}
-        >
-          Count: {count}
-        </Button>
-      </p>
-    );
-  }
   return (
     <div className="App">
-      {buttons}
-      <p>Hello mturk1</p>
+      <p>{prompt}</p>
+      <Button
+        onClick={(e) => {
+          setCount(count + 1);
+          setClickTimes([Date.now(), ...clickTimes]);
+        }}
+      >
+        Count: {count}
+      </Button>
     </div>
   );
 }
